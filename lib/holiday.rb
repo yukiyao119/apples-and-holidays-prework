f27ece64ec1756dee9ff513a780d9a7fa2334596
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-03 20:05:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,8 +42,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  
-  holiday_hash[season] = {}
+
   holiday_hash[season][:holiday_name] = supply_array
 
   # binding.pry 
