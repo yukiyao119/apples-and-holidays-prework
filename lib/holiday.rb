f98b1f53bd214a4ed9c90f50d3dc5444c10844bb
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-03 20:27:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,8 +81,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
+  result = []
   holiday_hash.collect do |season, holiday|
-    result = []
     holiday.each do |name, supplies|
         if supplies.include?"BBQ"
           result << name 
