ce6c7bbeb7eca81054737ea6a45a0313b9fb4d2d
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-03 20:14:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,8 +83,20 @@ def all_holidays_with_bbq(holiday_hash)
   
   
 end
-
-
+  # {
+  #   :winter => {
+  #     :christmas => ["Lights", "Wreath"],
+  #     :new_years => ["Party Hats"]
+  #   },
+  #   :summer => {
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
+  #   },
+  #   :fall => {
+  #     :thanksgiving => ["Turkey"]
+  #   },
+  #   :spring => {
+  #     :memorial_day => ["BBQ"]
+  #   }
 
 
 
